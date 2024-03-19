ce7bde8875ea8ca495a7b723be97e9ff5fcdc41f
khaled <ujjalbhuiyan123@gmail.com>
2024-03-20 00:10:29 +0600

    implemented user store logic

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -20,43 +20,29 @@ class UserRepository implements UserInterface
 
     public function saveUser($request)
     {
-        $user = [
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'password' => Hash::make($request->password),
-        ];
-
-        // return $user;
-         
         try {
             DB::beginTransaction();
-            // return $request->input('addresses');
-            // Create the user
+
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
                 'phone' => $request->phone,
                 'password' => Hash::make($request->password),
             ]);
-            $user->images()->create(['url' => FileUploadService::uploadFile($request->file('file'))]);
-            UserAddressCreated::dispatch($user, $request->input('addresses'));
+
+            FileUploadService::uploadFile($request->file, $user);
+            if ($request->has('addresses')) {
+                UserAddressCreated::dispatch($user, $request->input('addresses'));
+            }
         
             DB::commit();
         
-            return response()->json(['message' => 'User created successfully'], 201);
+            return $user;
         } catch (\Exception $e) {
-            // Rollback the transaction if an exception occurred
             DB::rollBack();
-        
-            // Log the error or handle it in any other appropriate way
             Log::error('Error creating user: ' . $e->getMessage());
-        
-            // Return an error response
             return response()->json(['error' => 'Failed to create user'], 500);
         }
-        
-        // return User::create($data);
     }
 
     public function updateUser($id, array $data)
