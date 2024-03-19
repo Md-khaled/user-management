3c29bf5e00897cd8ec76ad62a130e06930d5bc6c
khaled <ujjalbhuiyan123@gmail.com>
2024-03-20 00:12:51 +0600

    implemented file upload service logic

--- app/Services/FileUploadService.php
+++ app/Services/FileUploadService.php
@@ -3,15 +3,19 @@
 namespace App\Services;
 
 use App\Models\Image;
+use App\Models\User;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class FileUploadService
 {
-    public static function uploadFile($file)
+    private const STORAGE_TYPE = 'public';
+    public static function uploadFile($file, User $model)
     {
         $fileName = time() .'-'. Str::random(20) . '.' . $file->getClientOriginalExtension();
-        Storage::disk('public')->putFileAs(Image::STORAGE_PATH, $file, $fileName);
+        Storage::disk(self::STORAGE_TYPE)->putFileAs(Image::STORAGE_PATH, $file, $fileName);
+        $model->images()->create(['url' => Image::STORAGE_PATH. '/' . $fileName]);
+
         return Image::STORAGE_PATH. '/' . $fileName;
     }
 }
